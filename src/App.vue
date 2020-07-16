a69391615abb5b9bb1355a4ac86fdd1e54cea6fc
Paulina Grabowska <byrdziakp@gmail.com>
2020-07-16 21:34:33 +0200

    Added input and description

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <template>
   <div id="app">
+    <div class="main">
+        <h1 class="main__title">Weather app</h1>
+        <span class="main__sub-title">
+          Check the weather in Your city
+        </span>
+
+        <form action="" class="main__form">
+          <input v-model="city" type="text" name="city" class="main__input" placeholder="Write city name...">
+        </form>
+        <div class="main__result">
+          <div class="main__city"></div>
+          <div class="main__weather">
+            <div class="main__temp"></div>
+            <div class="main__weather-desc"></div>
+            <div class="main__pressure"></div>
+            <div class="main__wind"></div>
+          </div>
+        </div>
+
+    </div>
   </div>
 </template>
 
@@ -7,18 +27,16 @@
 
 export default {
   name: 'App',
+
   components: {
-  }
+  },
+
+  data () {
+    return {
+      city: '',
+    };
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"  src="./assets/style.scss" />
